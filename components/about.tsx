bd9e259cf4d2bc775f5d108c29028aa8442e76e5
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-03 13:11:12 +0200

    section-heading.tsx added, about component styled & animation added

--- components/about.tsx
+++ components/about.tsx
@@ -1,11 +1,20 @@
+"use client";
 
 import React from 'react'
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
 
 export default function About() {
   return (
-    <section>
-        <h2>About me</h2>
-        <p>
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+        initial={{opacity: 0, y: 100}}
+        animate={{opacity: 1, y: 0}}
+        transition={{
+            delay: 0.175,
+        }}
+    >
+        <SectionHeading>About me</SectionHeading>
+        <p className='mb-3'>
             After graduating with a degree in{" "}
             <span className="font-medium">Accounting</span>, I decided to pursue my
             passion for programming. I enrolled in a coding bootcamp and learned{" "}
@@ -14,6 +23,13 @@ export default function About() {
             problem-solving aspect. I <span className="underline">love</span> the
             feeling of finally figuring out a solution to a problem. My core stack
             is{" "}
+            <span className="font-medium">
+            React, Next.js, Node.js, and MongoDB
+            </span>
+            . I am also familiar with TypeScript and Prisma. I am always looking to
+            learn new technologies. I am currently looking for a{" "}
+            <span className="font-medium">full-time position</span> as a software
+            developer.
         </p>
         <p>
             <span className="italic">When I'm not coding</span>, I enjoy playing
@@ -23,6 +39,6 @@ export default function About() {
             <span className="font-medium">history and philosophy</span>. I'm also
             learning how to play the guitar. 
         </p>
-    </section>
+    </motion.section>
   )
 }
