c413e2ea1cc77cc2e966375ca89d1a683d119645
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-05 19:59:52 +0200

    custom hook implemented, clean the code in intro, about, project components with hook

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -2,8 +2,7 @@
 
 import React, { useState, createContext, useContext } from 'react'
 import { links } from '@/lib/data'
-
-type SectionName = typeof links[number]["name"]
+import type { SectionName } from '@/lib/types'
 
 type ActiveSectionContextProviderProvider = {
     children: React.ReactNode
