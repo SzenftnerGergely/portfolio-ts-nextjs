5df50a81f128ace3c5bdb1a4ad77aacd51f84d4b
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-06 10:37:58 +0200

    skills tsx added, component implemented, animation added

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
 import Project from './project'
@@ -11,7 +11,7 @@ export default function Projects() {
     const {ref} = useSectionInView("Projects", 0.5)
     
     return (
-        <section ref={ref} id='projects' className='scroll-mt-28'>
+        <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
             <SectionHeading>Projects</SectionHeading>
             <div>
                 {projectsData.map((project, index) => (
