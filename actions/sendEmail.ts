f736b9a5321db126644c3cafcc532cc279683b1c
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-08 19:18:25 +0200

    error handling func added into utils file

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,30 +1,38 @@
-"use server"
+"use server";
 
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils"
+import { validateString, getErrorMessage } from "@/lib/utils";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendEmail = async (formData:FormData) => {
-    const message = formData.get("message")
-    const senderEmail = formData.get("email")
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    if(!validateString(sendEmail, 500)) {
-        return {
-            error: "Invalid sender email"
-        }
+export const sendEmail = async (formData: FormData) => {
+  const message = formData.get("message");
+  const senderEmail = formData.get("senderEmail");
+
+  if (!validateString(sendEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "gergely.szenftner@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+        error: getErrorMessage(error)
     }
-    if(!validateString(message,5000)) {
-        return {
-            error: "Invalid sender email"
-        }
-    }
-
-    resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'gergely.szenftner@gmail.com',
-        subject: 'Message from contact form',
-        reply_to: senderEmail,
-        text: message
-    })
-}
+  }
+};
+ 
