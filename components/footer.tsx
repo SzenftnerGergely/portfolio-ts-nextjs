20dbebe8e09a19449f66a931cb1db09b4fde613d
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-09 12:55:11 +0200

    footer styled

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function footer() {
+export default function Footer() {
   return (
-    <footer>
-        <small>
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+        <small className='mb-2 text-xs block'>
             &copy; 2023 Gergely. All rights reserved.
         </small>
-        <p>
-            <span>About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+        <p className='text-xs'>
+            <span className='font-semibold'>About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
         </p>
     </footer>
   )
