96ca19eb9aca397bc3fa6c150c06022639c14200
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-09 12:47:43 +0200

    footer implemented

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <footer>
+        <small>
+            &copy; 2023 Gergely. All rights reserved.
+        </small>
+        <p>
+            <span>About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+        </p>
+    </footer>
+  )
+}
