8c4d3014d3f2f04857b99acd421461772a074b83
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-10 11:53:55 +0200

    responsivity finished

--- components/project.tsx
+++ components/project.tsx
@@ -28,11 +28,11 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         >
         <section className='bg-gray-100 max-w-[42rem] border 
         border-black/5 overflow-hidden sm:pr-8 relative
-        sm:h-[20rem] hover:bg-gray-200 group-even:pl-8
+        sm:h-[20rem] hover:bg-gray-200 sm:group-even:pl-8
         transition rounded-lg dark:bg-white/10 
         dark:hover:bg-white/20 dark:text-white'>
                 <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]
-                flex flex-col h-full group-even:ml-[18rem] '>
+                flex flex-col h-full sm:group-even:ml-[18rem] '>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
                     <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
                     <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -47,7 +47,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
                     src={imageUrl}
                     alt={title}
                     quality={95}
-                    className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg 
+                    className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg 
                     shadow-2xl group-even:-right[initial] group-even:-left-40
                     group-hover:-translate-x-3 group-hover:translate-y-3
                     group-hover:-rotate-2 group-hover:scale-[1.04] transition
